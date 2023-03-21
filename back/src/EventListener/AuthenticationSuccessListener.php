1fe75436e66d4f4e4dbf3d686be4b6db0b7ff239
jeremdh62 <jdhaille@myges.fr>
2023-03-21 20:24:24 +0100

    add verify user

---
+++ back/src/EventListener/AuthenticationSuccessListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class AuthenticationSuccessListener
+{
+
+
+/**
+ * @param AuthenticationSuccessEvent $event
+ */
+public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+{
+    $data = $event->getData();
+    $user = $event->getUser();
+
+    if (!$user instanceof UserInterface) {
+        return;
+    }
+
+    if (!$user->isIsVerify()) {
+        throw new \Exception('User is not active');
+    }
+}
+}
