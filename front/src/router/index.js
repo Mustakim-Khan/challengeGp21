14d26d191422e54bfdf044059f19852c2dab4bb2
Moufid17 <moufid.mtr@gmail.com>
2023-05-21 22:02:10 +0200

    [FRONT] store (refreshtoken, user datas) - router - authentification script.

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -14,9 +14,25 @@ import ForumView from "../views/ForumView.vue";
 import AdminForumsView from "../views/Admin/AdminForumsView.vue";
 import AdminForumEditView from "../views/Admin/AdminForumEditView.vue";
 import ForumEdit from "../views/ForumEditView.vue";
-//import store from "../store";
+import ArticleEdit from "../views/ArticleEditView.vue";
+import store from "../store";
 
-// TODO: Login redirect
+function adminRouteGuard(to, from, next) {
+  store.dispatch("fetchAccessToken");
+  if (store.state.authToken) {
+    if (store.state.user) {
+      let user = store.state.user;
+      if (
+        user.roles.includes("ROLE_MODERATOR") ||
+        user.roles.includes("ROLE_ADMIN")
+      ) {
+        next();
+      }
+    }
+  }
+  next({ name: "login" });
+}
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -24,65 +40,189 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
-      /*redirect:
-        store.state.user.id == "" || store.state.user.token == ""
-          ? "/login"
-          : "",*/
     },
     {
       path: "/admin",
       name: "admin",
       component: DashboardView,
+      beforeEnter: (to, from, next) => {
+        adminRouteGuard(to, from, next);
+      },
     },
     {
       path: "/admin/articles",
       name: "admin-articles",
       component: AdminArticlesView,
+      beforeEnter: (to, from, next) => {
+        adminRouteGuard(to, from, next);
+      },
     },
     {
       path: "/admin/articles/:id",
       name: "admin-article-edit",
       component: AdminArticleEditView,
+      beforeEnter: (to, from, next) => {
+        adminRouteGuard(to, from, next);
+      },
     },
     {
       path: "/admin/forums",
       name: "admin-forums",
       component: AdminForumsView,
+      beforeEnter: (to, from, next) => {
+        adminRouteGuard(to, from, next);
+      },
     },
     {
       path: "/admin/forums/:id",
       name: "admin-forum-edit",
       component: AdminForumEditView,
+      beforeEnter: (to, from, next) => {
+        adminRouteGuard(to, from, next);
+      },
     },
     {
-      path: "/login",
-      name: "login",
-      component: LoginView,
+      path: "/admin/tournaments",
+      name: "admin-tournaments",
+      component: AdminTournamentsView,
+      beforeEnter: (to, from, next) => {
+        store.dispatch("fetchAccessToken");
+        if (store.state.authToken == null || store.state.user == null) {
+          next("/login");
+        } else {
+          next();
+        }
+      },
+    },
+    {
+      path: "/admin/tournaments/:id",
+      name: "admin-tournament-edit",
+      component: AdminTournamentEditView,
+      beforeEnter: (to, from, next) => {
+        store.dispatch("fetchAccessToken");
+        if (store.state.authToken == null || store.state.user == null) {
+          next("/login");
+        } else {
+          next();
+        }
+      },
     },
     {
       path: "/article/:id",
       name: "article",
       component: () => import("../views/ArticleView.vue"),
+      beforeEnter: (to, from, next) => {
+        store.dispatch("fetchAccessToken");
+        if (store.state.authToken == null || store.state.user == null) {
+          next("/login");
+        } else {
+          next();
+        }
+      },
     },
     {
       path: "/articles",
       name: "articles",
       component: () => import("../views/ArticlesView.vue"),
+      beforeEnter: (to, from, next) => {
+        store.dispatch("fetchAccessToken");
+        if (store.state.authToken == null || store.state.user == null) {
+          next("/login");
+        } else {
+          next();
+        }
+      },
+    },
+    {
+      path: "/articles/edit/:id",
+      name: "article-edit",
+      component: ArticleEdit,
+      beforeEnter: (to, from, next) => {
+        store.dispatch("fetchAccessToken");
+        if (store.state.authToken == null || store.state.user == null) {
+          next("/login");
+        } else {
+          next();
+        }
+      },
     },
     {
       path: "/forum/:id",
       name: "forum",
       component: ForumView,
+      beforeEnter: (to, from, next) => {
+        store.dispatch("fetchAccessToken");
+        if (store.state.authToken == null || store.state.user == null) {
+          next("/login");
+        } else {
+          next();
+        }
+      },
     },
     {
       path: "/forums",
       name: "forums",
       component: ForumsView,
+      beforeEnter: (to, from, next) => {
+        store.dispatch("fetchAccessToken");
+        if (store.state.authToken == null || store.state.user == null) {
+          next("/login");
+        } else {
+          next();
+        }
+      },
     },
     {
       path: "/forums/edit/:id",
       name: "forum-edit",
       component: ForumEdit,
+      beforeEnter: (to, from, next) => {
+        store.dispatch("fetchAccessToken");
+        if (store.state.authToken == null || store.state.user == null) {
+          next("/login");
+        } else {
+          next();
+        }
+      },
+    },
+    {
+      path: "/tournaments",
+      name: "tournaments",
+      component: TournamentsView,
+      beforeEnter: (to, from, next) => {
+        store.dispatch("fetchAccessToken");
+        if (store.state.authToken == null || store.state.user == null) {
+          next("/login");
+        } else {
+          next();
+        }
+      },
+    },
+    {
+      path: "/tournament/:id",
+      name: "tournament",
+      component: TournamentView,
+      beforeEnter: (to, from, next) => {
+        store.dispatch("fetchAccessToken");
+        if (store.state.authToken == null || store.state.user == null) {
+          next("/login");
+        } else {
+          next();
+        }
+      },
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginView,
+      beforeEnter: (to, from, next) => {
+        store.dispatch("fetchAccessToken");
+        if (store.state.authToken != null || store.state.user != null) {
+          next("/home");
+        } else {
+          next();
+        }
+      },
     },
     {
       path: "/register",
@@ -90,25 +230,35 @@ const router = createRouter({
       component: RegisterView,
     },
     {
-      path: "/admin/tournaments",
-      name: "admin-tournaments",
-      component: AdminTournamentsView,
+      path: "/dashboard",
+      name: "user-dashboard",
+      component: () => import("../views/UserDashbordView.vue"),
+      beforeEnter: (to, from, next) => {
+        store.dispatch("fetchAccessToken");
+        if (!store.state.authToken || !store.state.user) {
+          next("/login");
+        }
+        next();
+      },
     },
     {
-      path: "/tournaments",
-      name: "tournaments",
-      component: TournamentsView,
+      path: "/reset/password",
+      name: "user-updatePassword",
+      component: () => import("../views/UpdatePasswordView.vue"),
     },
-    {
-      path: "/tournament/:id",
-      name: "tournament",
-      component: TournamentView,
-    },
-    {
-      path: "/admin/tournaments/:id",
-      name: "admin-tournament-edit",
-      component: AdminTournamentEditView,
-    },
+    // {
+    //   path: "/verify/email/:token",
+    //   name: "verify-email",
+    //   component: () => import("../views/EmailView.vue"),
+    //   beforeEnter: (to, from, next) => {
+    //     store.dispatch("fetchAccessToken");
+    //     if (!store.state.authToken || !store.state.user) {
+    //       next("/login");
+    //     } else {
+    //       next();
+    //     }
+    //   },
+    // },
     {
       path: "/:pathMatch(.*)*",
       redirect: "/",
@@ -116,4 +266,13 @@ const router = createRouter({
   ],
 });
 
+// router.beforeEach((to, from, next) => {
+//   if (!store.state.accessToken && to.name !== "Home") next({ name: "Login" });
+//   else next();
+// });
+
 export default router;
+
+// TODO :
+// Define route for : forums/edit/new
+//
--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,7 +1,9 @@
-/* eslint-disable prettier/prettier */
 import { createStore } from "vuex";
 import axios from "axios";
+import decryptToken from "../../utils/authAPI";
+// import { objectToJson } from "../../utils/utils";
 
+// Create instance of axios
 const instance = axios.create({
   baseURL: import.meta.env.VITE_URL_API,
   headers: {
@@ -10,22 +12,75 @@ const instance = axios.create({
   },
 });
 
-let authToken = localStorage.getItem("authToken");
+// Refresh token
+let refresh = false;
+instance.interceptors.response.use(
+  (response) => response,
+  async function (error) {
+    if (
+      error.response.status === 401 &&
+      refresh == false &&
+      localStorage.getItem("refreshToken")
+    ) {
+      console.log("Token expired. Refreshing token ...");
+      // Delete authorisation
+      delete instance.defaults.headers.common["Authorization"];
+      // Fetch refresh token route
+      const { status, data } = await instance.post("/api/token/refresh", {
+        refresh_token: localStorage.getItem("refreshToken"),
+      });
+      if (status === 200) {
+        // set refresh at true
+        refresh = true;
+        // Update instance authorisation with new token
+        instance.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${data.token}`;
+        // Update error authorization token
+        error.config.headers["Authorization"] = `Bearer ${data.token}`;
+        // update data in storage
+        localStorage.setItem("authToken", data.token);
+        console.log("Error config => ", error.config);
+        console.log(
+          "Error config token => ",
+          error.config.headers["Authorization"]
+        );
+        // const { status, data } = await instance.post("/api/token/refresh", {
+        //   refresh_token: localStorage.getItem("refreshToken"),
+        // });
+        console.log("Refresh token done.");
+        // Execute last request
+        return instance(error.config);
+      }
+    }
+    refresh = false;
+    return error;
+  }
+);
 
+// Fetch current user and his token
+let { tokenDecoded, userDecoded, refreshTokenDecoded } = await decryptToken();
+
+console.log(`Store data | User => ${userDecoded}`);
 instance.interceptors.request.use((config) => {
-  if (!authToken) {
-    authToken = "";
-  } else {
-    config.headers.Authorization = `Bearer ${authToken}`;
+  if (tokenDecoded) {
+    config.headers.Authorization = `Bearer ${tokenDecoded}`;
   }
   return config;
 });
 
 const store = createStore({
   state: {
-    user: undefined,
+    user: userDecoded,
     status: "",
-    authToken: authToken,
+    authToken: tokenDecoded,
+    refreshToken: refreshTokenDecoded,
+    userDatas: {},
+    userArticles: [],
+    userForums: [],
+    userComments: [],
+    userCommentsSignaled: [],
+    userTournaments: [],
     articles: [],
     forums: [],
     comments: [],
@@ -37,15 +92,50 @@ const store = createStore({
     },
     logUser: (state, auth) => {
       state.authToken = auth.token;
+      state.refreshToken = auth.refresh_token;
       localStorage.setItem("authToken", state.authToken);
-      state.user = { username: "moufiduser" };
-      instance.defaults.headers.common["Authorization"] = `Bearer ${state.authToken}`;
+      localStorage.setItem("refreshToken", state.refreshToken);
+      instance.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${state.authToken}`;
     },
     logout: (state) => {
-      // TODO: logout user
-      state.Authtoken = "";
-      localStorage.removeItem("user");
+      state.authToken = null;
+      state.user = {};
+      localStorage.removeItem("authToken");
+      localStorage.removeItem("refreshToken");
       delete instance.defaults.headers.common["Authorization"];
+    },
+    setUser: (state, userInfo) => {
+      state.user = userInfo;
+    },
+    updateUserToken: (state, accessToken) => {
+      state.authToken = accessToken;
+    },
+    setUserDatas: (state, userDatas) => {
+      if (userDatas != null) {
+        state.userDatas = userDatas;
+        const newUser = {
+          ...state.user,
+          ...userDatas[0],
+        };
+        state.user = newUser;
+      }
+    },
+    setUserArticles: (state, articles) => {
+      state.userArticles = articles ? articles : [];
+    },
+    setUserForums: (state, forums) => {
+      state.userForums = forums ? forums : [];
+    },
+    setUserComments: (state, comments) => {
+      state.userComments = comments ? comments : [];
+    },
+    setUserCommentsSignaled: (state, commentsSignaled) => {
+      state.userCommentsSignaled = commentsSignaled ? commentsSignaled : [];
+    },
+    setUserTournaments: (state, tournaments) => {
+      state.userTournaments = tournaments ? tournaments : [];
     },
     setArticles: (state, articles) => {
       state.articles = articles;
@@ -77,6 +167,28 @@ const store = createStore({
     },
     getAuthToken: (state) => {
       return state.authToken;
+    },
+    getCurrentUser: (state) => {
+      return state.user;
+    },
+    getUserDatas: (state) => {
+      console.log("STORE (getUserDatas) | ", state.userDatas);
+      return state.userDatas ? state.userDatas : null;
+    },
+    getUserArticles: (state) => {
+      return state.userArticles;
+    },
+    getUserForums: (state) => {
+      return state.userForums;
+    },
+    getUserTournaments: (state) => {
+      return state.userTournaments;
+    },
+    getUserComments: (state) => {
+      return state.userComments;
+    },
+    getUserCommentsSignaled: (state) => {
+      return state.userCommentsSignaled;
     },
     getArticles: (state) => {
       return state.articles;
@@ -120,7 +232,131 @@ const store = createStore({
             resolve(response);
           })
           .catch((error) => {
-            commit("setStatus", "errorLoggingIn");
+            commit("setStatus", "LoggingIn");
+            reject(error);
+          });
+      });
+    },
+    logout: ({ commit }) => {
+      // TODO: login user
+      commit("setStatus", "logout");
+      return new Promise((resolve) => {
+        commit("setStatus", "logout");
+        commit("logout");
+        resolve();
+      });
+    },
+    fetchAccessToken: ({ commit }) => {
+      const res = localStorage.getItem("authToken")
+        ? localStorage.getItem("authToken")
+        : null;
+      commit("updateUserToken", res);
+    },
+    updateCurrentUser: ({ commit }, data) => {
+      console.log(`Store (updateCurrentUser) | new user => ${data}`);
+      commit("setUser", data);
+    },
+    updateUserDatas: ({ commit }, userInfo) => {
+      // console.log(`STORE (updateUserDatas) | userInfo => ${userInfo.username}`);
+      return new Promise((resolve, reject) => {
+        instance
+          .get(`/api/users?username=${userInfo.username}`)
+          .then((response) => {
+            commit("setUserDatas", response.data);
+            resolve(response);
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      });
+    },
+    resetPassword: ({ commit }, { userId, userPwd }) => {
+      return new Promise((resolve, reject) => {
+        instance
+          .patch(
+            `/api/users/${userId}/reset/password`,
+            { password: userPwd },
+            { headers: { "Content-Type": "application/merge-patch+json" } }
+          )
+          .then((response) => {
+            commit("status", "reset password done.");
+            resolve(response);
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      });
+    },
+    forgetPassword: ({ commit }, userEmail) => {
+      return new Promise((resolve, reject) => {
+        instance
+          .get(`/api/users/forget/password`, { userEmail })
+          .then((response) => {
+            commit("status", "forget password done.");
+            resolve(response);
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      });
+    },
+    getUserArticles: ({ commit }, userId) => {
+      return new Promise((resolve, reject) => {
+        instance
+          .get(`/api/users/${userId}/articles`)
+          .then((response) => {
+            commit("setUserArticles", response.data);
+            resolve(response);
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      });
+    },
+    getUserForums: ({ commit }, userId) => {
+      return new Promise((resolve, reject) => {
+        instance
+          .get(`/api/users/${userId}/forums`)
+          .then((response) => {
+            commit("setUserForums", response.data);
+            resolve(response);
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      });
+    },
+    getUserTournaments: ({ commit }, userId) => {
+      return new Promise((resolve, reject) => {
+        instance
+          .get(`/api/users/${userId}/tournaments`)
+          .then((response) => {
+            commit("setUserTournaments", response.data);
+            resolve(response);
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      });
+    },
+    getUserComments: ({ commit }, userId) => {
+      return new Promise((resolve, reject) => {
+        instance
+          .get(`/api/users/${userId}/comments`)
+          .then((response) => {
+            const signaled = [];
+            if (response.data) {
+              response.data.forEach((element) => {
+                if (element.signaledComments) {
+                  signaled.push(...element.signaledComments);
+                }
+              });
+            }
+            commit("setUserComments", response.data);
+            commit("setUserCommentsSignaled", signaled);
+            resolve(response);
+          })
+          .catch((error) => {
             reject(error);
           });
       });
@@ -156,6 +392,7 @@ const store = createStore({
         instance
           .post("/api/articles", article)
           .then((response) => {
+            console.log("Create Article => ", response.data);
             commit;
             resolve(response);
           })
